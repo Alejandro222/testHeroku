e423d1ab7e9c3c9140919ee419623e9640383f8d
ale97 <presaguirre@gmail.com>
2017-07-31 19:51:33 -0600

    add link user

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('https://testheroku-2016.herokuapp.com/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
