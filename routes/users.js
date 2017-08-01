ae7bdd64d7dbfca9d77d7fef775276ea89c79a36
Michael <msalasch97@gmail.com>
2017-07-31 19:23:05 -0600

    crud for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,75 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
+var criteria = function(req) {
+  return { where: {id: req.params.id} };
+}
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.format({
+      json: function () {
+        models.users.findAll().then(users => {
+          res.json({ users: users });
+        });
+      },
+      html: function () {
+        models.users.all().then(users => {
+          res.render('users/index', { users: users });
+        });
+      }
+    });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new');
+});
+
+
+router.post('/', function (req, res, next) {
+  var user = models.users.create(req.body);
+  res.format({
+    json: function () {
+      user.then(user => {
+        res.json(user);
+      });
+    },
+    html: function () {
+      user.then(user => {
+        res.redirect('/users');
+      });
+    }
+  })
+});
+
+router.delete('/:id', function (req, res, next) {
+    res.format({
+      json: function () {
+        models.users.destroy(criteria(req)).then(() => {
+          res.json({ status: 'ok' });
+        });
+      },
+      html: function () {
+        models.users.destroy(criteria(req)).then(() => {
+          res.redirect('/users');
+        });
+      }
+    });
+});
+
+router.put('/:id', function (req, res, next) {
+    res.format({
+      json: function () {
+        models.users.update(req.body, criteria(req)).then(user => {
+          res.json(user);
+        });
+      },
+      html: function () {
+        models.users.update(req.body, criteria(req)).then(user => {
+          res.redirect('/users');
+        });
+      }
+    });
 });
 
 module.exports = router;
